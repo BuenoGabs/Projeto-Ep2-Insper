eb3a1cb8ec5697b6ffa16e6b1ebd6c0e313ba9fa
BuenoGabs <gabrielb6@al.insper.edu.br>
2021-11-04 01:27:26 -0300

    Função cria peças

--- dev1.py
+++ dev1.py
@@ -1,1 +1,11 @@
-print('gabriel')
+def cria_pecas():
+    from random import shuffle
+    lista_domin = []
+    for x in range (0,7):
+        for y in range(0, 7):
+            l = [x,y]
+            lista_invert = [y,x]
+            if lista_invert not in lista_domin:
+                lista_domin.append(l)
+    shuffle(lista_domin)
+    return lista_domin
