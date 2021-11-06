92cfcc73ba0870c78fcade4c2f0ea3905f2a992c
BuenoGabs <gabrielb6@al.insper.edu.br>
2021-11-06 10:09:18 -0300

    Interando dominó

--- jogo_principal.py
+++ jogo_principal.py
@@ -14,6 +14,23 @@ continuar = True
 while continuar:
     pecas = cria_pecas()
     status = inicia_jogo(n_jogadores, pecas)
+    nao_ganhador = True
+    while nao_ganhador:
+        for i in range(n_jogadores):
+            if i == 0:
+                mao_jogador = status['jogadores'][0]
+                lista_mesa =  status['mesa']
+                posicoes = posicoes_possiveis(lista_mesa, mao_jogador)
+                quantidade = len(mao_jogador)
+                print('MESA: ', lista_mesa)
+                print(f'Você tem {quantidade} peça(s)')
+                escolha_do_jogador = input('Escolha a peça: ')
+
+            
+                
+
+
+
 
     
     
