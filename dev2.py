8191f66c1148508a6121cda1733b09c0a1c1846b
Fernandayukimi <fernandayrk.al.insper.edu.br>
2021-11-07 21:56:22 -0300

    jogo

--- dev2.py
+++ dev2.py
@@ -74,3 +74,34 @@ def adiciona_na_mesa(p, lista_mesa):
             lista_mesa.append(p_invert)
 
     return lista_mesa
+
+# jogo dominó:
+from dev2 import *
+import random
+
+texto_inicial = '''
+Insper Dominó
+=> Design de Software
+Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar 
+sem peças na sua mão antes dos outros jogadores.
+Vamos começar!!!
+'''
+print(texto_inicial)
+
+n_jogadores = int(input('Quantos jogadores?[2/4]'))
+continuar = True
+while continuar:
+    pecas = cria_pecas()
+    status = inicia_jogo(n_jogadores, pecas)
+    nao_ganhador = True
+    while nao_ganhador:
+        for i in range(n_jogadores):
+            if i == 0:
+                mao_jogador = status['jogadores'][0]
+                lista_mesa = status['mesa']
+                posicoes = posicoes_possiveis(lista_mesa, mao_jogador)
+                qunatidade = len(mao_jogador)
+                print(f'Você tem {qunatidade} peças(s)')
+                escolha_do_jogador = input('Escolha a peça:')
+        if verifica_ganhador(jogadores) != -1:
+            continuar == False
